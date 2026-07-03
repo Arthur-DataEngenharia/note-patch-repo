d12af00c65d6b5afeef472257ffc5a50f6d74a3b
ARTHUR.RODRIGUES <arthur.rodrigues@data.com.br>
2026-07-02 23:21:21 -0300

    revert: undo sidebar grouping, restore original nav layout

--- src/App.tsx
+++ src/App.tsx
@@ -200,7 +200,6 @@ export default function App() {
             </Suspense>
           }
         />
-        <Route path="/management" element={<Navigate to="/archive" replace />} />
         <Route
           path="/archive"
           element={
